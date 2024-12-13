95d98cffd6d81807d4cc1f4b422494c619dc0972
Ezequiel Purro <ezequiel.purro@gmail.com>
2024-12-12 21:43:13 -0300

    homepage

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,9 +1,19 @@
 
 const HomePage = () => {
   return (
-    <div>
-      HomePage
-    </div>
+    <>
+      <div className="row">
+        <div className="col-12">
+          <div className="image-content">
+            <img src="https://www.designsystem.es/wp-content/uploads/2024/07/organizacion_salon_belleza.jpg" />
+            <div className="overlay">
+              <h1>NUEVA IDENTIDAD <br/> COWORKING</h1>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+    </>
   )
 }
 
