30fbf1f9969dbe2dc592b7d72bb461af41235745
Ezequiel Purro <ezequiel.purro@gmail.com>
2024-12-13 12:16:50 -0300

    avances en el home

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -7,11 +7,18 @@ const HomePage = () => {
           <div className="image-content">
             <img src="https://www.designsystem.es/wp-content/uploads/2024/07/organizacion_salon_belleza.jpg" />
             <div className="overlay">
-              <h1>NUEVA IDENTIDAD <br/> COWORKING</h1>
+              <div className="titulo">
+                <h1>NUEVA IDENTIDAD <br/> COWORKING</h1>
+                <p>Un espacio para vos</p>
+              </div>
             </div>
           </div>
         </div>
-        
+      </div>
+      <div className="row">
+        <div className="col-lg-12 subtitulo">
+          <p>Nuestros servicios</p>
+        </div>
       </div>
     </>
   )
