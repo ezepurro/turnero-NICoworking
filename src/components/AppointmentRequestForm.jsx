5948fb66145b73a59f0588866b78923c801ceafc
Ezequiel Purro <ezequiel.purro@gmail.com>
2025-01-05 23:12:57 -0300

    Changes in AppointmentRequestForm

--- src/components/AppointmentRequestForm.jsx
+++ src/components/AppointmentRequestForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import DatePicker, {registerLocale} from "react-datepicker";
 import es from 'date-fns/locale/es'
 
@@ -9,7 +9,34 @@ registerLocale('es', es);
 
 const AppointmentRequestForm = () => {
 
-    const [startDate, setStartDate] = useState(new Date());
+    const [startDate, setStartDate] = useState();
+
+    const [selectedOption, setSelectedOption] = useState('');
+
+    const handleChange = (event) => {
+        setSelectedOption(event.target.value);
+    };
+  
+    const excludeDates = useMemo(() => {
+        const dates = [];
+        const enabledDates = 5;
+        const currentDate = new Date();
+        while (currentDate.getDay() !== enabledDates) {
+            currentDate.setDate(currentDate.getDate() + 1);
+        }
+        for (let i = 0; i < 5; i++) {
+            dates.push(new Date(currentDate)); 
+            currentDate.setDate(currentDate.getDate() + 7); 
+        }
+        return dates;
+    }, []); 
+
+    const filterTime = useMemo(() => {
+        return (time) => {
+            const hour = time.getHours();
+            return hour >= 9 && hour < 17;
+        };
+    }, []);
 
     return (
     <div className='container text-center'>
@@ -17,14 +44,27 @@ const AppointmentRequestForm = () => {
             <div className="col-12">
                 <form action="" className='appointment-form'>
                     <input type="text" placeholder='Número de contacto' className='form-control' />
+                    <select id="options" value={selectedOption} onChange={handleChange} className='form-control'>
+                        <option value="" disabled>Seleccione la cantidad de zonas</option>
+                        <option value="1">1 Zona</option>
+                        <option value="3">3 Zonas</option>
+                        <option value="5">5 Zonas</option>
+                        <option value="10">Full-body</option>
+                    </select>
                     <DatePicker
                         selected={ startDate }
+                        placeholderText='Seleccione una fecha y hora'
                         className="form-control"
-                        onChange={(date) => setStartDate(date)}
+                        onChange={ (date) => setStartDate(date) }
                         dateFormat="Pp"
                         showTimeSelect
                         locale="es"
                         timeCaption="Hora"
+                        onKeyDown={ (e) => { e.preventDefault() } }
+                        minDate={ new Date() }
+                        includeDates={ excludeDates }
+                        filterTime={ filterTime } 
+                        timeIntervals={ 15 }
                     />
                     <button type='submit' className='form-control'>Reservar turno</button>
                 </form>
