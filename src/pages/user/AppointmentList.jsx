3787557ac11fa2cf7f0db1694204163e4613e7f7
Ezequiel Purro <ezequiel.purro@gmail.com>
2025-02-10 14:12:16 -0300

    appointment history added

--- src/pages/user/AppointmentList.jsx
+++ src/pages/user/AppointmentList.jsx
@@ -1,32 +1,54 @@
+import { useState } from 'react';
 import { convertDateToDDMMYY, convertDateToHHMM } from '../../helpers/converters';
-import '../../styles/components/appointmentList.css';
 import Appointment from './Appointment';
 import NoAppointments from './NoAppointments';
-
+import '../../styles/components/appointmentList.css';
 
 const AppointmentList = ({ name, appointments }) => {
+  const [selectedOption, setSelectedOption] = useState(true);
+
+  const futureAppointments = appointments.filter(
+    (appointment) => new Date(appointment.date) > new Date()
+  );
+
+  const displayedAppointments = selectedOption ? futureAppointments : appointments;
+
+  const sortedAppointments = [...displayedAppointments].sort(
+    (a, b) => new Date(a.date) - new Date(b.date)
+  );
 
   return (
     <div className="container">
-        <div className="row">
-            <div className="col-12 appointments-container">
-                <h3 className='text-center'>Mis turnos</h3>
-                <h5 className='text-center'>{name}</h5>
-                {
-                  (appointments.length !== 0)
-                    ? (appointments.map( appointment => {
-                      return <Appointment
-                        key={appointment.id}
-                        service={appointment.type}
-                        date={convertDateToDDMMYY(appointment.date)}
-                        hour={convertDateToHHMM(appointment.date)} />
-                    }))
-                    : (<NoAppointments />)
-                }
-            </div>
-        </div>
-    </div>
-  )
-}
+      <div className="row">
+        <div className="col-12 appointments-container">
+          <h3 className="text-center">
+            {selectedOption ? "Mis próximos turnos" : "Mi historial de turnos"}
+          </h3>
+          <h5 className="text-center">{name}</h5>
 
-export default AppointmentList
+          {sortedAppointments.length !== 0 ? (
+            sortedAppointments.map((appointment) => (
+              <Appointment
+                key={appointment.id}
+                service={appointment.type}
+                date={convertDateToDDMMYY(appointment.date)}
+                hour={convertDateToHHMM(appointment.date)}
+              />
+            ))
+          ) : (
+            <NoAppointments />
+          )}
+
+          <button
+            onClick={() => setSelectedOption(!selectedOption)}
+            className='btn'
+          >
+            {selectedOption ? "Ver historial de turnos" : "Ver próximos turnos"}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AppointmentList;
