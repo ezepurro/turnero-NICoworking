51fce8b80b50159efacfc435528a4179734b453e
Ezequiel Purro <ezequiel.purro@gmail.com>
2025-03-25 21:53:03 -0300

    Navbar imports fixed

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -1,5 +1,5 @@
-import Navbar from "../../components/navbar";
 import Footer from "../../components/Footer";
+import Navbar from "../../components/Navbar";
 import LoginForm from "./login/LoginForm";
 
 const LoginPage = () => {
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,4 +1,4 @@
-import Navbar from "../../components/Navbar";
+import Navbar from "../../components/Navbar.jsx";
 import MainTitle from "./MainTitle";
 import Promos from "./Promos";
 import Services from "./Services";
