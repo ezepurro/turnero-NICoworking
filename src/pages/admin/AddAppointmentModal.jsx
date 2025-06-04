012699ea5560ccff6c44a10abec1948cf84778f7
Ezequiel Purro <ezequiel.purro@gmail.com>
2025-06-04 00:45:22 -0300

    admin datepicker bug fixed

--- src/pages/admin/AddAppointmentModal.jsx
+++ src/pages/admin/AddAppointmentModal.jsx
@@ -66,16 +66,22 @@ const AddAppointmentModal = ({ show, handleClose, handleSubmit, formData, setFor
 
   const handleDateChange = async (date) => {
     setFormData(prev => ({ ...prev, date }));
-    setStartDate(date)
+    setStartDate(date);
     const sessionLength = (parseInt(formData.sessionZones) !== 10)
       ? parseInt(formData.sessionZones) * 5
       : 25;
 
     const { reservedTimes, startTime, endTime } = await getReservedTimes(date, sessionLength);
+    
+    const newStartTime = new Date(startTime);
+    newStartTime.setHours(newStartTime.getHours() - 3); // Ajuste de zona horaria a UTC-3
+    const newEndTime = new Date(endTime);
+    newEndTime.setHours(newEndTime.getHours() - 3); // Ajuste de zona horaria a UTC-3
+
     setExcludedTimes(reservedTimes);
     setTimeRange({
-      start: startTime ? new Date(startTime) : null,
-      end: endTime ? new Date(endTime) : null
+      start: newStartTime ? new Date(newStartTime) : null,
+      end: newEndTime ? new Date(newEndTime) : null
     });
   }
 
